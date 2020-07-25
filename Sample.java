bdba3bcbbf0444fd648733ef02f4382b4762c235
prajvalkavuri <46378347+prajvalkavuri@users.noreply.github.com>
2020-07-25 20:47:01 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,21 @@
+public class Sample{
+
+
+     public static void main(String []args){
+
+         int var1=5;
+
+         int var2=5;
+
+         int var3=var1+var2;
+	   
+	   int var4= var1*var2;
+
+        System.out.println("Sum :"+var3);
+        
+        System.out.println("Multiplication :"+var4);
+
+
+     }
+
+}
